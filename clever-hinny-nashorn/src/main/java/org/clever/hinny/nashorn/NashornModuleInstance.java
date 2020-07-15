c83f86c35a4abff67fc565b719fb00ecb299c879
lizw <1183409807@qq.com>
2020-07-15 22:58:41 +0800

    依赖配置

---
+++ clever-hinny-nashorn/src/main/java/org/clever/hinny/nashorn/NashornModuleInstance.java
@@ -0,0 +1,58 @@
+package org.clever.hinny.nashorn;
+
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+import org.clever.hinny.api.ModuleInstance;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2020/07/15 22:39 <br/>
+ */
+public class NashornModuleInstance implements ModuleInstance<ScriptObjectMirror> {
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public String getFilename() {
+        return null;
+    }
+
+    @Override
+    public boolean isLoaded() {
+        return false;
+    }
+
+    @Override
+    public ModuleInstance<ScriptObjectMirror> getParent() {
+        return null;
+    }
+
+    @Override
+    public List<String> paths() {
+        return null;
+    }
+
+    @Override
+    public Set<ModuleInstance<ScriptObjectMirror>> getChildren() {
+        return null;
+    }
+
+    @Override
+    public ScriptObjectMirror getExports() {
+        return null;
+    }
+
+    @Override
+    public ScriptObjectMirror require(String id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ScriptObjectMirror getModuleInstance() {
+        return null;
+    }
+}
---
+++ clever-hinny-nashorn/src/test/java/org/clever/hinny/nashorn/NashornTest01.java
@@ -0,0 +1,16 @@
+package org.clever.hinny.nashorn;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2020/07/15 22:54 <br/>
+ */
+@Slf4j
+public class NashornTest01 {
+    @Test
+    public void t01() {
+        log.info("### --->");
+    }
+}

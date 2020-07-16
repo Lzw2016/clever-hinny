2906c01165210ab8eb3d09959efd55bad5f48266
lizw <lzw1000000@163.com>
2020-07-16 18:17:27 +0800

    add MemoryModuleCache

---
+++ clever-hinny-api/src/main/java/org/clever/hinny/api/module/MemoryModuleCache.java
@@ -0,0 +1,52 @@
+package org.clever.hinny.api.module;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import org.clever.hinny.api.ScriptObject;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Module 内存缓存
+ */
+public class MemoryModuleCache<T extends ScriptObject> implements ModuleCache<T> {
+    /**
+     * 缓存
+     */
+    private final Cache<String, ModuleInstance<T>> modulesCache;
+
+    /**
+     * @param clearTimeInterval 定时清除缓存的时间间隔，单位：秒(小于0表示不清除)
+     */
+    public MemoryModuleCache(long clearTimeInterval) {
+        CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder().initialCapacity(521);
+        if (clearTimeInterval >= 0) {
+            cacheBuilder.expireAfterWrite(clearTimeInterval, TimeUnit.SECONDS);
+        }
+        this.modulesCache = cacheBuilder.build();
+    }
+
+    public MemoryModuleCache() {
+        this(-1);
+    }
+
+    @Override
+    public ModuleInstance<T> get(String fullPath) {
+        return modulesCache.getIfPresent(fullPath);
+    }
+
+    @Override
+    public void put(String fullPath, ModuleInstance<T> moduleInstance) {
+        modulesCache.put(fullPath, moduleInstance);
+    }
+
+    @Override
+    public void clear() {
+        modulesCache.invalidateAll();
+    }
+
+    @Override
+    public void remove(String fullPath) {
+        modulesCache.invalidate(fullPath);
+    }
+}

f676284c9a10dc19df8775991468380a75b11a00
lizw <lzw1000000@163.com>
2020-07-20 09:51:48 +0800

    微调

--- clever-hinny-nashorn/src/test/resources/javascript/03使用npm包/src/01.js
+++ clever-hinny-nashorn/src/test/resources/javascript/03使用npm包/src/01.js
@@ -1,13 +1,13 @@
-var _trim = require('lodash/trim');
+// var _trim = require('lodash/trim');
 // var lodash = require('lodash');
 // var moment = require('moment');
-// var dayjs = require('dayjs');
+var dayjs = require('dayjs');
 
 var trim = function (str) {
     // print("require -> ", require.toString())
-    print("_trim -> ", _trim('  a123  '))
+    // print("_trim -> ", _trim('  a123  '))
     // print("moment  -> ", moment("2020-07-19").toDate())
-    // print("dayjs   -> ", dayjs('2018-05-05').locale('zh-cn').format())
+    print("dayjs   -> ", dayjs('2018-05-05').locale('zh-cn').format())
     return str.trim(str);
     // return lodash.trim(str);
 }

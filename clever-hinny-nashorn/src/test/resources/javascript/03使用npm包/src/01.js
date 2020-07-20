ed49ea2a63b69d132b7b97921e511a3e6c8e9b85
lizw <lzw1000000@163.com>
2020-07-20 15:50:31 +0800

    add 三方js库

--- clever-hinny-nashorn/src/test/resources/javascript/03使用npm包/src/01.js
+++ clever-hinny-nashorn/src/test/resources/javascript/03使用npm包/src/01.js
@@ -1,12 +1,25 @@
 // var _trim = require('lodash/trim');
 // var lodash = require('lodash');
-// var moment = require('moment');
+// var underscore = require('underscore');
+// var Sugar = require('sugar');    // 不可用
+// var Lazy = require('lazy.js');   // 不可用
+// var R = require('ramda');        // 不可用
+// var Immutable = require('immutable');
+// var numeral = require('numeral');
+// var bigJs = require('big.js');   // 不可用
+// var cryptoJs = require('crypto-js');
 var dayjs = require('dayjs');
+// var moment = require('moment');
 
 var trim = function (str) {
-    // print("require -> ", require.toString())
-    // print("_trim -> ", _trim('  a123  '))
-    // print("moment  -> ", moment("2020-07-19").toDate())
+    // print("require -> ", require.toString());
+    // print("_trim -> ", _trim('  a123  '));
+    // print("underscore -> ", underscore.keys({one: 1, two: 2, three: 3}));
+    // print("Sugar -> ", Sugar.Array.unique([1, 2, 2, 3]));
+    // print("Lazy -> ", Lazy([6, 18, 2, 49, 34]).min());
+    // print("ramda -> ", R.add(7)(10));
+    // print("Immutable -> ", Immutable.Map({a: 1, b: 2, c: 3}));
+    // print("moment  -> ", moment("2020-07-19").toDate());
     print("dayjs   -> ", dayjs('2018-05-05').locale('zh-cn').format())
     return str.trim(str);
     // return lodash.trim(str);

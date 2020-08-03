49372c39fff5ac498c01bd4dd614de3c3dd7f6d4
lizw <lzw1000000@163.com>
2020-08-03 13:15:52 +0800

    微调

--- clever-hinny-graaljs/src/main/java/org/clever/hinny/graaljs/utils/JavaInteropUtils.java
+++ clever-hinny-graaljs/src/main/java/org/clever/hinny/graaljs/utils/JavaInteropUtils.java
@@ -1,10 +1,8 @@
 package org.clever.hinny.graaljs.utils;
 
 import lombok.extern.slf4j.Slf4j;
-import org.clever.hinny.graaljs.internal.GraalJObject;
 import org.graalvm.polyglot.Value;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -104,112 +102,4 @@ public class JavaInteropUtils {
 //        }
 //        return res;
 //    }
-
-    // ------------------------------------------------------------------------------------------------------------------ JS -> Java
-
-    /**
-     * 把JavaScript对象转换成Java对象 <br />
-     * 1. 只考虑简单对象和“数组”、“Object对象” <br />
-     * 2. 只做浅转换(一层转换) <br />
-     *
-     * @param value JavaScript对象
-     */
-    public Object toJavaObject(Value value) {
-        if (value == null) {
-            return null;
-        }
-        Object object = toJavaObjectForBase(value);
-        if (object != value) {
-            return object;
-        }
-        // 数组, 对象
-        if (value.hasArrayElements()) {
-            long size = value.getArraySize();
-            if (size > Integer.MAX_VALUE) {
-                throw new ClassCastException("数组 arg.length=" + size + " 太长无法转换");
-            }
-            Object[] array = new Object[(int) size];
-            for (int i = 0; i < size; i++) {
-                array[i] = toJavaObjectForBase(value.getArrayElement(i));
-            }
-            return array;
-        } else {
-            Set<String> keys = value.getMemberKeys();
-            Map<String, Object> map = new HashMap<>(keys.size());
-            for (String key : keys) {
-                map.put(key, toJavaObjectForBase(value.getMember(key)));
-            }
-            return map;
-        }
-    }
-
-//    public Object[] toJavaObject(Object[] array) {
-//        Collection<?> collection = toJavaObject(Arrays.asList(array));
-//        // TODO
-//        return null;
-//    }
-//
-//    public Collection<?> toJavaObject(Collection<?> collection) {
-//        // TODO
-//        return null;
-//    }
-//
-//    public Map<?, ?> toJavaObject(Map<?, ?> map) {
-//        // TODO
-//        return null;
-//    }
-
-    /**
-     * 把JavaScript对象转换成Java对象 <br />
-     * 1. 只考虑简单对象，不考虑“数组”、“Object对象” <br />
-     * 2. 只做浅转换(一层转换) <br />
-     *
-     * @param value JavaScript对象
-     */
-    protected Object toJavaObjectForBase(Value value) {
-        if (value == null || value.isNull()) {
-            return null;
-        } else if (value.isHostObject()) {                                      // Java对象
-            return value.asHostObject();
-        } else if (value.isException()) {                                       // 异常
-            return value.throwException();
-        } else if (value.isBoolean()) {                                         // boolean
-            return value.asBoolean();
-        } else if (value.isNumber()) {
-            if (value.fitsInByte()) {                                           // byte
-                return value.asByte();
-            }
-            if (value.fitsInShort()) {                                          // short
-                return value.asShort();
-            }
-            if (value.fitsInInt()) {                                            // int
-                return value.asInt();
-            }
-            if (value.fitsInLong()) {                                           // long
-                return value.asLong();
-            }
-            if (value.fitsInFloat()) {                                          // float
-                return value.asFloat();
-            }
-            if (value.fitsInDouble()) {                                         // double
-                return value.asDouble();
-            }
-            return value.asDouble();
-        } else if (value.isString()) {                                          // String
-            return value.asString();
-        } else if (value.isDate() || value.isTime() || value.isInstant()) {     // Date 日期+时间
-            return GraalJObject.Instance.asJDate(value);
-        } else if (value.isDuration()) {                                        // Duration 时间段
-            return value.asDuration();
-        } else if (value.isTimeZone()) {                                        // Duration 时间段
-            return value.asTimeZone();
-        } else if (value.isProxyObject()) {                                     // Proxy 对象
-            return value.asProxyObject();
-        } else if (value.isMetaObject()) {                                      // Class 类型
-            return value.getMetaObject();
-        } else if (value.isNativePointer()) {                                   // 本机指针
-            return value;
-        }
-        return value;
-    }
 }

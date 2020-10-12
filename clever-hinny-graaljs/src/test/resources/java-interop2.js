5e2876bb4b3d1b8fd5c46adf04ed03d011db65a7
lizw <lzw1000000@163.com>
2020-10-12 10:11:25 +0800

    优化

--- clever-hinny-graaljs/src/test/resources/java-interop2.js
+++ clever-hinny-graaljs/src/test/resources/java-interop2.js
@@ -6,8 +6,13 @@ console.log("# -> getProxyMap2: | ", map);
 console.log("# -> getProxyMap2: | ", map.string);
 console.log("# -> getProxyMap2: | ", Interop.asJMap(map));
 
-// var list = Interop.asJList(map, map, map, map);
 var list = JavaInterop.getProxyMap3();
 console.log("# -> list: | ", list);
 JavaInterop.setValue(list);
 JavaInterop.setList2(list);
+
+list = Interop.asJList(map, map, map, map);
+console.log("# -> list: | ", list);
+JavaInterop.setValue(list);
+
+
